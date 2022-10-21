8e7e31697662d67d90d7c5dc2432252672deaa70
vantheman2210 <martinovic.ba@gmail.com>
2022-10-21 10:13:29 +0200

    Creating chat

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,55 +1,63 @@
 import React, { useState, createContext, useEffect } from 'react';
 import { auth, db } from './Firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
 
 export const AppContext = createContext();
 
 export const Provider = (props) => {
-	
-	const [userId, setUserId] = useState();
-	const [posts, setPosts] = useState([]);
-  
+	const [ userId, setUserId ] = useState();
+	const [ posts, setPosts ] = useState([]);
+	const [ contacts, setContacts ] = useState([]);
 
 	// Checking wether user is signed in
-  useEffect(() => {
+	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, async (user) => {
 			if (user) {
 				// If signed in set user id and fetch posts
 				setUserId(user.uid);
-				console.log('hello')
-				 fetchPosts(user.uid);
+				console.log('hello');
+				fetchPosts(user.uid);
+				getContacts();
 			} else {
 				// User is signed out
 				// remove user id and posts
-        setUserId();
+				setUserId();
 				setPosts([]);
+				setContacts([]);
 				console.log(user);
 			}
 		});
 		return unsubscribe;
 	}, []);
 
-
 	// Fetch posts to be able to display them on user page
-	const fetchPosts = async (id) => { 
-		const querySnapshot = await getDocs(collection(db, "posts", id, "userPosts"));
-		querySnapshot.forEach((doc) => { 
-			console.log(doc.data())
-			setPosts((prev) => [...prev, doc.data()])
-			
-		})
-	}
+	const fetchPosts = async (id) => {
+		const querySnapshot = await getDocs(collection(db, 'posts', id, 'userPosts'));
+		querySnapshot.forEach((doc) => {
+			console.log(doc.data());
+			setPosts((prev) => [ ...prev, doc.data() ]);
+		});
+	};
 
+	const getContacts = async () => {
+		const q = query(collection(db, 'users'), where('id', '==', userId));
+
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach((doc) => {
+			// doc.data() is never undefined for query doc snapshots
+			console.log(doc.data().friends);
+			setContacts((prev) => [ ...prev, doc.data().friends ]);
+		});
+	};
 
 	// Delete this later
-	useEffect(() => { 
-		console.log(posts)
-	},[posts])
-
-	return (
-		<AppContext.Provider value={[ userId, posts ]}>
-			{props.children}
-		</AppContext.Provider>
+	useEffect(
+		() => {
+			console.log(posts);
+		},
+		[ posts ]
 	);
+
+	return <AppContext.Provider value={[ userId, posts, contacts ]}>{props.children}</AppContext.Provider>;
 };
--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from '../Firebase';
-import { addDoc, collection, Timestamp, doc } from 'firebase/firestore';
+import { addDoc, collection, Timestamp, doc, getDocs, query, where } from 'firebase/firestore';
 import { AppContext } from '../AppContext';
 import { useContext } from 'react';
 import '../styles/Messages.css';
 
 const Messages = () => {
-	const [ userId ] = useContext(AppContext);
+	const [ userId, posts, contacts ] = useContext(AppContext);
+  
 
-	const createChat = () => {
+	const createChat = async () => {
 		const docRef = doc(db, 'messages', userId);
 		const colRef = collection(docRef, 'chats');
 
 		// Adding post doc to firebase cloud with photo url, caption, and date
-		addDoc(colRef, {
-			time: Timestamp.now().toDate()
-		});
+		try {
+			await addDoc(colRef, {
+				time: Timestamp.now().toDate()
+			});
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
-	return <div />;
+	return (
+		<div className="chatContainer">
+			<div className="contacts"> 
+        <p>Contacts:</p>
+        {contacts.map(contact =>  
+          return (
+            <div key={i}>{contact}</div>
+          )
+        }
+      </div>
+			<div className="chatMessages">
+				<div className="container">
+					<img src="/w3images/bandmember.jpg" alt="Avatar" />
+					<p>Hello. How are you today?</p>
+					<span className="time-right">11:00</span>
+				</div>
+				<div className="container darker">
+					<img src="/w3images/avatar_g2.jpg" alt="Avatar" className="right" />
+					<p>Hey! I'm fine. Thanks for asking!</p>
+					<span className="time-left">11:01</span>
+				</div>
+				<div className="container">
+					<img src="/w3images/bandmember.jpg" alt="Avatar" />
+					<p>Sweet! So, what do you wanna do today?</p>
+					<span className="time-right">11:02</span>
+				</div>
+				<div className="container darker">
+					<img src="/w3images/avatar_g2.jpg" alt="Avatar" className="right" />
+					<p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+					<span className="time-left">11:05</span>
+				</div>
+        <form className="msger-inputarea">
+    <input type="text" className="msger-input" placeholder="Enter your message..."/>
+    <button type="submit" className="msger-send-btn">Send</button>
+  </form>
+			</div>
+		</div>
+	);
 };
 
 export default Messages;
